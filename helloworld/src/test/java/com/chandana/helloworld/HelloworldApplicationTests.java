80f94f01e847919bdaff6b4f9007ca8f0a0283d8
AlexLacasta13 <78559705+AlexLacasta13@users.noreply.github.com>
2022-02-04 19:19:18 +0100

    Add files via upload

---
+++ helloworld/src/test/java/com/chandana/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chandana.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
